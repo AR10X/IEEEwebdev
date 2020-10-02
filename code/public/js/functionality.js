1dc62cc3dec964b14a450c5d1a06c20176210d16
Abdul Rehaman <59757602+AR10X@users.noreply.github.com>
2020-10-03 00:28:03 +0530

    Add home page with navs and form

---
+++ code/public/js/functionality.js
@@ -0,0 +1,10 @@
+
+/* Set the width of the side navigation to 400px */
+function openNav() {
+    document.getElementById("sidenav").style.width = "400px";
+  }
+  
+  /* Set the width of the side navigation to 0 */
+  function closeNav() {
+    document.getElementById("sidenav").style.width = "0";
+  }
