4d06ca22c94c2d4ea7c57cecc6d544d3fe46bb3c
Abdul Rehaman <59757602+AR10X@users.noreply.github.com>
2020-10-04 11:17:38 +0530

    oct 4 morining commit

--- code/public/js/functionality.js
+++ code/public/js/functionality.js
@@ -78,3 +78,44 @@ $(document).ready(function() {
   }
   hidePreloader();
   });
+
+   /* ---------------------------------------------------
+    FROM REDIRECT
+----------------------------------------------------- */
+
+const fromPlanet = {
+  Mars: "from_mars.html",
+  Earth: "from_earth.html",
+  Moon: "from_moon.html",
+  UFA:"from_uns.html"
+};
+
+var dataBase = {
+  fromPlace:""
+};
+
+function fromLinker(dataBase,fromPlanet){
+   var x = document.getElementById("from-first-form").selectedIndex;
+   var fromPlaceValue = document.getElementsByTagName("option")[x].value;
+   alert(fromPlaceValue);
+   alert(dataBase.fromPlace);
+   dataBase.fromPlace = fromPlaceValue;
+   
+   for(const property in fromPlanet){
+     if(property == fromPlaceValue){
+       var Linkvalue = fromPlanet.property;
+     }
+   }
+   alert(Linkvalue);
+   document.getElementById("firstlink").href = Linkvalue;
+};
+
+
+
+
+function fromSettlement(){
+  var y = document.getElementById("settlement-selection").selectedIndex;
+  var settlementValue = document.getElementsByTagName("option")[y].value;
+  alert(settlementValue);
+  document.getElementById("from-settlement-name").value = settlementValue;
+};
